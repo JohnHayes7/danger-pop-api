ce0f19686891b5899bd61b4c21335cbf37659369
John Hayes <JohnnyHayes7@gmail.com>
2021-03-08 22:47:10 -0500

    updated index and create actions for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
+
     
     def index
         users = User.all
         render json: UserSerializer.new(users)
+    end
+
+    def create
+        user = User.new(user_params)
+        user.password = params[:password]
+        if user.save
+            render json: UserSerializer(user)
+        else
+            render json: {code: 2020, message: "Could Not Create Account.  Please confirm your information"}
+        end
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :email, :phone_number, :password)
     end
 
 
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
+  has_one :cart
   attributes :id, :name, :email, :phone_number, :administrator, :tattoo_approved
 end
