255c0a9ef17b9393f808e7c22f0ffdf6f7521f4c
John Hayes <JohnnyHayes7@gmail.com>
2021-03-08 23:06:51 -0500

    added store_id to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_034806) do
+ActiveRecord::Schema.define(version: 2021_03_09_040441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_034806) do
     t.boolean "tattoo_approved"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "store_id"
   end
 
 end
