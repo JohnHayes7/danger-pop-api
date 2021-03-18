0a0452efe395578f64baec4ed25adff31ab27b52
John Hayes <JohnnyHayes7@gmail.com>
2021-03-18 10:55:08 -0400

    added title to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_191008) do
+ActiveRecord::Schema.define(version: 2021_03_18_145207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_191008) do
     t.boolean "project_complete_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
   create_table "stores", force: :cascade do |t|
