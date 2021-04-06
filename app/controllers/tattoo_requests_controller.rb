409f386c33c15b751bfcc6d63c63755b1544c0f1
John Hayes <JohnnyHayes7@gmail.com>
2021-04-06 17:22:09 -0400

    debugging file uploads

--- app/controllers/tattoo_requests_controller.rb
+++ app/controllers/tattoo_requests_controller.rb
@@ -4,4 +4,21 @@ class TattooRequestsController < ApplicationController
         trs = TattooRequest.all
         render json: TattooRequestsSerializer.new(trs)
     end
+
+    def create
+        binding.pry
+        @tr = TattooRequest.new(tattoo_request_params)
+
+        if @tr.save
+            render json: @tr
+        else
+            render json: @tr.errors.full_messages
+        end
+    end
+
+    private
+
+    def tattoo_request_params
+        params.permit(:guest_email, :description, :image)
+    end
 end
