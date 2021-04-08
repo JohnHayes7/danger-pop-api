f5414f2dad130566ced52133079e43bdb97cbdc7
John Hayes <JohnnyHayes7@gmail.com>
2021-04-08 12:54:05 -0400

    added validations to tattoo requests

--- app/models/tattoo_request.rb
+++ app/models/tattoo_request.rb
@@ -3,6 +3,8 @@ class TattooRequest < ApplicationRecord
     has_one :project
     has_many :notes
 
+
+
     has_attached_file :image, styles:{
         thumb: '100x100>',
         square: '200x200#',
@@ -10,4 +12,8 @@ class TattooRequest < ApplicationRecord
       }
 
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+    validates :guest_email, presence: true
+    validates :description, presence: true 
+    validates :allergies, presence: true
+    validates :body_location_image_path, presence:true
 end
