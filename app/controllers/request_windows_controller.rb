18120faf33c82c95c9165d77e2f8d6ea35e05131
John Hayes <JohnnyHayes7@gmail.com>
2021-06-08 16:48:00 -0400

    added toggle and saved state of the tattoo request form

--- app/controllers/request_windows_controller.rb
+++ app/controllers/request_windows_controller.rb
@@ -1,2 +1,17 @@
 class RequestWindowsController < ApplicationController
+
+
+    def show
+        window = RequestWindow.find(params[:id])
+        render json: window
+
+    end
+
+    def update
+        window = RequestWindow.find(params[:id])
+        window.openState = params[:open]
+        window.save
+        render json: window
+    end
+
 end
