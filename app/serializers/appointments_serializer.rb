fb75fbf3cc8bacb435a14a60cb3f18afeeaa9698
John Hayes <JohnnyHayes7@gmail.com>
2021-06-14 10:35:51 -0400

    added attributes to serializer

--- app/serializers/appointments_serializer.rb
+++ app/serializers/appointments_serializer.rb
@@ -4,6 +4,6 @@ class AppointmentsSerializer
   belongs_to :project
   has_one :user, through: :project
   has_one :tattoo_request, through: :project
-  attributes :project, :artist, :user, :tattoo_request, :date, :time, :artist_id, :project_id
+  attributes :project, :artist, :user, :tattoo_request, :date, :time, :daypart, :length_time, :artist_id, :project_id
 
 end
