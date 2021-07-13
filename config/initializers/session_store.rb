7331e1b45ccf51e49f0954d811ceb351470ecd6a
John Hayes <JohnnyHayes7@gmail.com>
2021-07-13 16:31:08 -0400

    debugging sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,7 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_danger-pop-api', domain: 'http://localhost:3000'
+    Rails.application.config.session_store :cookie_store, key: '_danger-pop-api'
   else
     Rails.application.config.session_store :cookie_store, key: '_danger-pop-api' 
   end
+
+  # , domain: 'http://localhost:3000'
