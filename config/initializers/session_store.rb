adf3ebd6e2fbdb1b0d353927ee7e4b415c6f3434
John Hayes <JohnnyHayes7@gmail.com>
2021-07-13 15:29:43 -0400

    updated session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_your-app-name', domain: 'your-frontend-domain'
+    Rails.application.config.session_store :cookie_store, key: '_danger-pop-api', domain: 'http://localhost:3000'
   else
     Rails.application.config.session_store :cookie_store, key: '_danger-pop-api' 
   end
