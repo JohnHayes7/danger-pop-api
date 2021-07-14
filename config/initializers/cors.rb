5c64084c8df035ac0581fe6c7bb10af177997786
John Hayes <JohnnyHayes7@gmail.com>
2021-07-14 13:56:03 -0400

    debugging sessions

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
     allow do
-      origins 'http://localhost:3000', 'https://www.dangerpopstudio.com/'
+      origins 'http://localhost:3000'
     
       resource '*',
         headers: :any,
