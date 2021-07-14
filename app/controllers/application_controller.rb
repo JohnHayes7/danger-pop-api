46808530c33882fd2bcb578aeae1312a573b0887
John Hayes <JohnnyHayes7@gmail.com>
2021-07-14 19:00:52 -0400

    debugging sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
     end
 
     def current_user
-        puts "current_user_session: #{session[:user_id]}"
+        puts "current_user_session: #{session.to_hash}"
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
