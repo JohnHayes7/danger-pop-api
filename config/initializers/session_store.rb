b33f6fc09681acbcec1e5692c2a3229a2e9c3619
John Hayes <JohnnyHayes7@gmail.com>
2021-07-14 11:51:35 -0400

    debugging sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_danger-pop-api', domain: 'danger-pop-api.herokuapp.com'
+    Rails.application.config.session_store :cookie_store, key: '_danger-pop-api', domain: 'danger-pop-api.herokuapp.com', secure: true
   else
     Rails.application.config.session_store :cookie_store, key: '_danger-pop-api' 
   end
