5d0fedd61c0efbd0af1d3796055beb5465708561
John Hayes <JohnnyHayes7@gmail.com>
2021-07-13 21:23:24 -0400

    debugging sessions

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module DangerPopApi
 
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
-    config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore)
+    # config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore)
 
     # config.middleware.insert_before 0, Rack::Cors do 
     #   allow do 
