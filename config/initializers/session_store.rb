83b8c413597d8a64cc195bb984151d893e57f267
John Hayes <JohnnyHayes7@gmail.com>
2021-07-15 14:55:08 -0400

    adding same_site none

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_danger-pop-api', domain: 'danger-pop-api.herokuapp.com', secure: true
+    Rails.application.config.session_store :cookie_store, key: '_danger-pop-api', domain: 'danger-pop-api.herokuapp.com', secure: true, same_site: :none
   else
     Rails.application.config.session_store :cookie_store, key: '_danger-pop-api' 
   end
