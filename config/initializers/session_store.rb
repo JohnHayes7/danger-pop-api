1578c0e21fb5dc734fa1db3d36f4d90c32025131
John Hayes <JohnnyHayes7@gmail.com>
2021-07-16 10:24:31 -0400

    debugging same site lax

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -9,11 +9,14 @@
   # else
   #   Rails.application.config.session_store :cookie_store, key: '_danger-pop-api' 
   # end
-
-  Rails.application.config.session_store :cookie_store, {
-  :key => '_danger-pop-api',
-  :domain => :all,
-  :same_site => :none,
-  :secure => :true,
-  :tld_length => 2
-}
+if Rails.env == 'production'
+    Rails.application.config.session_store :cookie_store, {
+    :key => '_danger-pop-api',
+    :domain => :all,
+    :same_site => :none,
+    :secure => :true,
+    :tld_length => 2
+  }
+else
+  Rails.application.config.session_store :cookie_store, key: '_danger-pop-api' 
+end
