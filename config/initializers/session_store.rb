30e735952464ec42681b5b93e6ff7ad642354a26
John Hayes <JohnnyHayes7@gmail.com>
2021-07-16 10:21:05 -0400

    debugging cors

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,19 @@
-if Rails.env == 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_danger-pop-api',  secure: Rails.env.production?
-else
-  Rails.application.config.session_store :cookie_store, key: '_danger-pop-api' 
-end
+# if Rails.env == 'production' 
+#   Rails.application.config.session_store :cookie_store, key: '_danger-pop-api',  secure: Rails.env.production?
+# else
+#   Rails.application.config.session_store :cookie_store, key: '_danger-pop-api' 
+# end
 
   # if Rails.env == 'production' 
   #   Rails.application.config.session_store :cookie_store, key: '_danger-pop-api', domain: 'danger-pop-api.herokuapp.com', secure: true, :same_site => "none"
   # else
   #   Rails.application.config.session_store :cookie_store, key: '_danger-pop-api' 
   # end
+
+  Rails.application.config.session_store :cookie_store, {
+  :key => '_danger-pop-api',
+  :domain => :all,
+  :same_site => :none,
+  :secure => :true,
+  :tld_length => 2
+}
