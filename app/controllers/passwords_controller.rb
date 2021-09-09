61dcecbbac42bb9163486d3e1d576c730312c379
John Hayes <JohnnyHayes7@gmail.com>
2021-09-09 14:23:53 -0400

    updated error messaging for password resets

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -24,16 +24,20 @@ class PasswordsController < ApplicationController
         if params[:email].blank?
           return render json: {error: 'Token not present'}
         end
+
         user = User.find_by(reset_password_token: token)
+        if !user
+          return render json: {status: 'fail', error: 'Token Error: Please ensure token is accurate.  Tokens are case sensitive.  Best practice is to copy and paste from your reset email'}
+        end
         
         if user.present? && user.password_token_valid?
-            
           if user.reset_password!(params[:password])
             
             render json: {status: 'ok'}, status: :ok
           else
             render json: {error: user.errors.full_messages}, status: :unprocessable_entity
           end
+
         else
           render json: {error:  'Link not valid or expired. Try generating a new link.'}, status: :not_found
         end
